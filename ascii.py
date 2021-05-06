7f77d705cc554712a4b67a6e80161e09886f81c9
Arian Omidi <arian.omidi@icloud.com>
2021-05-06 17:47:54 -0600

    ASCII video converter finished and added progress bar

--- ascii.py
+++ ascii.py
@@ -4,137 +4,147 @@ import numpy as np
 from scipy.ndimage.filters import gaussian_filter, percentile_filter
 from scipy import ndimage
 import math
+from pathlib import Path
 import matplotlib.pyplot as plt
-  
+
 from PIL import Image, ImageFilter, ImageFont, ImageDraw, ImageOps
 
 from ansi import *
-  
-# gray scale level values from: 
+
+# gray scale level values from:
 # http://paulbourke.net/dataformats/asciiart/
-  
+
 # 70 levels of gray
 gscale1 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
-  
+
 # 10 levels of gray
-gscale2 = '@%#*+=-:. '
+gscale2 = "@%#*+=-:. "
+# gscale2 = "ARIANomidi"
 
 # color codes schemes
 color_schemes = {
-    'binary' : ['\033[37m'],
-    'grayscale_4b' : [ '\033[97m', '\033[37m', '\033[90m', '\033[0m'],
-    'grayscale_3' : [ '\033[38;5;255m', '\033[38;5;248m', '\033[38;5;241m', '\033[0m'],
-    'grayscale_5' : [ '\033[38;5;255m', '\033[38;5;250m', '\033[38;5;244m', '\033[38;5;239m', '\033[38;5;235m', '\033[0m' ],
-    
-    'rgb_colorful' : [ '\033[37m', '\033[33m', '\033[31m', '\033[34m', '\033[0m'],
-    'rgb_cool' : [ '\033[37m', '\033[95m', '\033[35m', '\033[34m', '\033[0m']
+    "binary": ["\033[37m"],
+    "grayscale_4b": ["\033[0m", "\033[90m", "\033[37m", "\033[97m"],
+    "grayscale_3": ["\033[0m", "\033[38;5;241m", "\033[38;5;248m", "\033[38;5;255m"],
+    "grayscale_5": [
+        "\033[38;5;234m",
+        "\033[38;5;235m",
+        "\033[38;5;239m",
+        "\033[38;5;244m",
+        "\033[38;5;250m",
+        "\033[38;5;255m",
+    ],
+    "rgb_colorful": ["\033[0m", "\033[34m", "\033[31m", "\033[33m", "\033[37m"],
+    "rgb_cool": ["\033[0m", "\033[34m", "\033[35m", "\033[95m", "\033[37m"],
 }
+
 
 def normalizeTiles(tiles):
     """
     Given numpy array, filter and return normalized image array
     """
-    print(tiles.min(), tiles.max())
-    
+    """print(tiles.min(), tiles.max())"""
+
     # percentage filter
     min_val = np.percentile(tiles, 0)
     max_val = np.percentile(tiles, 100)
     tiles = np.clip(tiles, min_val, max_val)
-    
-    print(tiles.min(), tiles.max())
-    
+
+    """print(tiles.min(), tiles.max())"""
+
     # normalize tile value array
     normalized_tiles = (tiles - min_val) / (max_val - min_val)
-    
-    return normalized_tiles
-    
-def filterImage(image):
-    # return image
 
-    return image.filter(ImageFilter.DETAIL)
+    return normalized_tiles
+
+
+def filterImage(image):
+    return image
+    # return image.filter(ImageFilter.DETAIL)
+
     # ------ todo: remove ------
     # f = plt.figure()
     # f.add_subplot(1,2, 1)
     # plt.imshow(im, cmap='gray', vmin=0, vmax=255)
-    
+
     # im = ndimage.median_filter(im, 3)
-    
+
     # f.add_subplot(1, 2, 2)
     # plt.imshow(block_mean, cmap='gray', vmin=0, vmax=255)
     # plt.show(block=True)
     # ------ todo: remove ------
-  
-def covertImageToAscii(fileName, colors, cols, scale, moreLevels, invertImg):
+
+
+def covertImageToAscii(image, colors, cols, scale, moreLevels, invertImg):
     """
-    Given Image and dims (rows, cols) returns an m*n list of Images 
+    Given Image and dims (rows, cols) returns an m*n list of Images
     """
     # declare globals
     global gscale1, gscale2
-  
-    # open image and convert to grayscale
-    image = Image.open(fileName).convert('L')
+
+    # filter image
     image = filterImage(image)
-  
+
     # store dimensions
     W, H = image.size[0], image.size[1]
-    print(" - input image dims: %d x %d" % (W, H))
-  
+    """print(" - input image dims: %d x %d" % (W, H))"""
+
     # compute width of tile
     w = W / cols
-  
+
     # compute tile height based on aspect ratio and scale
     h = w / scale
-  
+
     # compute number of rows
-    rows = int(H/h)
-      
-    print(" - cols: %d, rows: %d" % (cols, rows))
-    print(" - tile dims: %d x %d" % (w, h))
-  
+    rows = int(H / h)
+
+    """print(" - cols: %d, rows: %d" % (cols, rows))
+    print(" - tile dims: %d x %d" % (w, h))"""
+
     # check if image size is too small
     if cols > W or rows > H:
         print("Image too small for specified cols!")
         exit(0)
-    
+
     # get image as numpy array
     im = np.array(image)
     im = im.transpose()
-        
-    # apply filter
-    if (invertImg):
-        im = 255 - im
 
+    # apply inversion
+    if invertImg:
+        im = 255 - im
+        colors = list(reversed(colors))
 
     """
     Given numpy array of a b/w image, get the average value tile array
     """
     avgs = []
-    
+
     for j in range(rows):
         row_avgs = []
-        y1 = int(j*h)
-        y2 = int((j+1)*h)
-        
+        y1 = int(j * h)
+        y2 = int((j + 1) * h)
+
         # correct last tile
-        if j == rows-1:
+        if j == rows - 1:
             y2 = H
-        
+
         for i in range(cols):
-            x1 = int(i*w)
-            x2 = int((i+1)*w)
-            
+            x1 = int(i * w)
+            x2 = int((i + 1) * w)
+
             # correct last tile
-            if i == cols-1:
+            if i == cols - 1:
                 x2 = W
-            
+
             # get avg brightness array of tile
-            avg = np.median(im[x1:x2,y1:y2])
+            avg = np.median(im[x1:x2, y1:y2])
             row_avgs.append(avg)
-        
-        avgs.append(row_avgs)  
-    
+
+        avgs.append(row_avgs)
+
     tiles = normalizeTiles(np.array(avgs))
-  
+
     # ascii image is a list of character strings
     aimg = []
     # color image is a list of ansi color codes strings
@@ -144,24 +154,24 @@ def covertImageToAscii(fileName, colors, cols, scale, moreLevels, invertImg):
         # append an empty string
         aimg.append("")
         cimg.append([])
-        
+
         for i in range(cols):
             # get character representation of tile
             gsval = ""
             char_ratio = tiles[j][i]
-            
+
             if moreLevels:
                 gsval += gscale1[int(69 * char_ratio)]
             else:
-                gsval += gscale2[int(9 * char_ratio)]   
-                
+                gsval += gscale2[int(9 * char_ratio)]
+
             # get coresponding color of tile
-            color_index = int((len(colors)-1) * tiles[j][i])
-            cimg[j].append(colors[color_index])     
-            
+            color_index = int((len(colors) - 1) * tiles[j][i])
+            cimg[j].append(colors[color_index])
+
             # append ascii char to string
-            aimg[j] += gsval    
-  
+            aimg[j] += gsval
+
     # return txt image
     return aimg, cimg
 
@@ -170,6 +180,7 @@ def covertImageToAscii(fileName, colors, cols, scale, moreLevels, invertImg):
 
 WIDTH_SCALING_FACTOR = 0.75
 HEIGHT_SCALING_FACTOR = 0.84
+
 
 def text_image(aimg, cimg, inverted, font_path=None):
     """Convert text file to a grayscale image with black characters on a white background.
@@ -180,52 +191,80 @@ def text_image(aimg, cimg, inverted, font_path=None):
     """
     # declare globals
     global WIDTH_SCALING_FACTOR, HEIGHT_SCALING_FACTOR
-    
+
     # # parse the file into lines
     # with open(text_path) as text_file:  # can throw FileNotFoundError
     #     lines = tuple(l.rstrip() for l in text_file.readlines())
     lines = aimg
-    
+
     # choose a font (you can see more detail in my library on github)
     large_font = 20  # get better resolution with larger size
-    font_path = font_path or 'fonts/SFMono-Medium.otf'  # Courier New. works in windows. linux may need more explicit path
+    font_path = (
+        font_path or "fonts/SFMono-Medium.otf"
+    )  # Courier New. works in windows. linux may need more explicit path
     try:
         font = ImageFont.truetype(font_path, size=large_font)
     except IOError:
         font = ImageFont.load_default()
-        print('Could not use chosen font. Using default.')
+        print("Could not use chosen font. Using default.")
 
     # make the background image based on the combination of font and lines
-    pt2px = lambda pt: int(round(pt * 96.0 / 72))   # function that converts points to pixels
-    max_width_line = max(lines, key=lambda s: font.getsize(s)[0])   # get line with largest width
+    pt2px = lambda pt: int(
+        round(pt * 96.0 / 72)
+    )  # function that converts points to pixels
+    max_width_line = max(
+        lines, key=lambda s: font.getsize(s)[0]
+    )  # get line with largest width
     # max height is adjusted down because it's too large visually for spacing
-    test_string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    test_string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     max_height = pt2px(font.getsize(test_string)[1])
     max_width = pt2px(font.getsize(max_width_line)[0])
     char_width = round(max_width / len(max_width_line))
-    height = int(max_height * len(lines) * HEIGHT_SCALING_FACTOR) # perfect or a little oversized
-    width = int(max_width * WIDTH_SCALING_FACTOR) # a little oversized
+    height = int(
+        max_height * len(lines) * HEIGHT_SCALING_FACTOR
+    )  # perfect or a little oversized
+    width = int(max_width * WIDTH_SCALING_FACTOR)  # a little oversized
     image = Image.new("RGB", (width, height), color=background_color(inverted))
     draw = ImageDraw.Draw(image)
 
     # draw each line of text
     vertical_position = 0
     horizontal_position = 0
-    line_spacing = round(max_height * HEIGHT_SCALING_FACTOR)  # reduced spacing seems better
+    line_spacing = round(
+        max_height * HEIGHT_SCALING_FACTOR
+    )  # reduced spacing seems better
     char_spacing = round(char_width * WIDTH_SCALING_FACTOR)
-    
+
     for line, line_colors in zip(lines, cimg):
         hor_pos = horizontal_position
         for c, color in zip(line, line_colors):
             rbg_color = ansi_rgb(color)
-            draw.text((hor_pos, vertical_position),
-                    c, fill=rbg_color, font=font)
-            hor_pos += char_spacing 
+            draw.text((hor_pos, vertical_position), c, fill=rbg_color, font=font)
+            hor_pos += char_spacing
         vertical_position += line_spacing
-    
+
     return image
 
+
 # ---------------------------------------- #
+
+
+def convertImageToAscii(
+    image,
+    colors=color_schemes["grayscale_5"],
+    cols=80,
+    scale=0.5,
+    moreLevels=True,
+    invertImg=True,
+):
+    """
+    Converts given image to an ASCII image
+    """
+    # get text and ANSI colors of image
+    aimg, cimg = covertImageToAscii(image, colors, cols, scale, moreLevels, invertImg)
+    # convert to image
+    return text_image(aimg, cimg, invertImg)
+
 
 # main() function
 def main():
@@ -233,85 +272,98 @@ def main():
     descStr = "This program converts an image into ASCII art."
     parser = argparse.ArgumentParser(description=descStr)
     # add expected arguments
-    parser.add_argument('imgFile')
-    
-    parser.add_argument('-g', '--greyscale', dest='greyscaleScheme', type=int, choices=[0, 1, 2, 3], default=3)
-    parser.add_argument('-c', '--color', dest='colorScheme', type=int, choices=[0, 1])
-    parser.add_argument('-n', '--cols', dest='cols', type=int, required=False)
-    parser.add_argument('-l', '--scale', dest='scale', type=float, required=False)
-    parser.add_argument('-m', '--morelevels', dest='moreLevels', action='store_true')
-    parser.add_argument('-i', '--invert', dest='inverted', action='store_false')
-    
-    parser.add_argument('-o', '--out', dest='outFile', required=False)
-    parser.add_argument('-O', '--imgout', dest='imgOutFile', required=False)
-    parser.add_argument('-H', '--hide', dest='hide', action='store_true')
-    parser.add_argument('-s', '--save', dest='save', action='store_true')
-    parser.add_argument('-p', '--print', dest='print', action='store_true')
-    
+    parser.add_argument("imgFile")
+
+    parser.add_argument(
+        "-g",
+        "--greyscale",
+        dest="greyscaleScheme",
+        type=int,
+        choices=[0, 1, 2, 3],
+        default=3,
+    )
+    parser.add_argument("-c", "--color", dest="colorScheme", type=int, choices=[0, 1])
+    parser.add_argument("-n", "--cols", dest="cols", type=int, required=False)
+    parser.add_argument("-l", "--scale", dest="scale", type=float, required=False)
+    parser.add_argument("-m", "--morelevels", dest="moreLevels", action="store_true")
+    parser.add_argument("-i", "--invert", dest="inverted", action="store_false")
+
+    parser.add_argument("-o", "--out", dest="outFile", required=False)
+    parser.add_argument("-O", "--imgout", dest="imgOutFile", required=False)
+    parser.add_argument("-H", "--hide", dest="hide", action="store_true")
+    parser.add_argument("-s", "--save", dest="save", action="store_true")
+    parser.add_argument("-p", "--print", dest="print", action="store_true")
+
     # parse args
     args = parser.parse_args()
-    
-    imgFile = args.imgFile
-  
+
+    # open image and convert to grayscale
+    imgFile = Path(args.imgFile)
+    if imgFile.exists:
+        image = Image.open(args.imgFile).convert("L")
+    else:
+        print("ERROR : Image does not exist.")
+        exit(0)
+
     # set text output file
-    outFile = 'out.txt'
+    outFile = "out.txt"
     if args.outFile:
         outFile = args.outFile
-        
+
     # set img output file
-    filename = imgFile.split('/')[-1].split('.')[0]
-    imgOutFile = 'out/' + filename + '_ascii.png'
+    imgOutFile = Path("out") / "{}_ascii.png".format(imgFile.stem)
     if args.imgOutFile:
         imgOutFile = args.imgOutFile
-  
+
     # set scale default as 0.5 which suits
     # a Courier font
     scale = 0.5
     if args.scale:
         scale = float(args.scale)
-  
+
     # set cols
     cols = 80
     if args.cols:
         cols = int(args.cols)
-        
+
     # set color scheme
-    if (args.colorScheme == 0):
-        colors = color_schemes['rgb_colorful']
-    elif (args.colorScheme == 1):
-        colors = color_schemes['rgb_cool']
-    elif (args.greyscaleScheme == 0):
-        colors = color_schemes['binary']
-    elif (args.greyscaleScheme == 1):
-        colors = color_schemes['grayscale_4b']
-    elif (args.greyscaleScheme == 2):
-        colors = color_schemes['grayscale_3']
-    elif (args.greyscaleScheme == 3):
-        colors = color_schemes['grayscale_5']
-    
-        
+    if args.colorScheme == 0:
+        colors = color_schemes["rgb_colorful"]
+    elif args.colorScheme == 1:
+        colors = color_schemes["rgb_cool"]
+    elif args.greyscaleScheme == 0:
+        colors = color_schemes["binary"]
+    elif args.greyscaleScheme == 1:
+        colors = color_schemes["grayscale_4b"]
+    elif args.greyscaleScheme == 2:
+        colors = color_schemes["grayscale_3"]
+    elif args.greyscaleScheme == 3:
+        colors = color_schemes["grayscale_5"]
+
     # -------------------------------------- #
-    print('generating ASCII art...')
+    print("generating ASCII art...")
     start = time.perf_counter()
-    
+
     # convert image to ascii txt
-    aimg, cimg = covertImageToAscii(imgFile, colors, cols, scale, args.moreLevels, args.inverted)
-  
+    aimg, cimg = covertImageToAscii(
+        image, colors, cols, scale, args.moreLevels, args.inverted
+    )
+
     # write to text file
-    f = open(outFile, 'w')
+    f = open(outFile, "w")
     for line, line_colors in zip(aimg, cimg):
         for c, color in zip(line, line_colors):
             f.write(color + c)
-        f.write("\n")        
+        f.write("\n")
     f.write("\033[0m")
     f.close()
-    
+
     # print output
     if args.print:
-        f = open(outFile, 'r')
-        print(f.read()) 
+        f = open(outFile, "r")
+        print(f.read())
         f.close()
-    
+
     # make image from text
     image = text_image(aimg, cimg, args.inverted)
     # save/show the image
@@ -319,11 +371,11 @@ def main():
         image.show()
     if args.save or args.imgOutFile:
         image.save(imgOutFile)
-    
+
     end = time.perf_counter()
     print(f"Completed {end - start:0.4f} seconds")
-    
-  
+
+
 # call main
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- video-conversion.py
+++ video-conversion.py
@@ -2,8 +2,21 @@ import cv2
 import time
 import os
 from pathlib import Path
+from PIL import Image
+import numpy as np
+from tqdm import tqdm, trange
 
-from ascii import covertImageToAscii, text_image, color_schemes
+from ascii import convertImageToAscii, text_image, color_schemes
+
+
+def convertOpenCV2PIL(img_cv):
+    img_pil = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+    return Image.fromarray(img_pil)
+
+
+def convertPIL2OpenCV(img_pil):
+    img_arr = np.array(img_pil)
+    return cv2.cvtColor(img_arr, cv2.COLOR_RGB2BGR)
 
 
 def img_to_vid(in_path, out, fps):
@@ -54,14 +67,87 @@ def vid_to_img(filename, out):
     cv2.destroyAllWindows()
 
 
+def convertVideoToAscii(filename, out):
+    fps = 18
+    colors = color_schemes["grayscale_5"]
+    cols = 80
+    scale = 0.5
+    moreLevels = True
+    invert = True
+
+    # init video capture
+    cam = cv2.VideoCapture(str(filename))
+    total_frames = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(cam.get(cv2.CAP_PROP_FPS))
+
+    # init output video
+    success, frame = cam.read()
+    frame_pil = convertOpenCV2PIL(frame).convert("L")
+    ascii_img = convertImageToAscii(frame_pil, colors, cols, scale, moreLevels, invert)
+    width, height = ascii_img.size[0], ascii_img.size[1]
+    video = cv2.VideoWriter(
+        str(out), cv2.VideoWriter_fourcc(*"mp4v"), fps, (width, height)
+    )
+
+    # init progress bar
+    with tqdm(total=total_frames, ncols=75, unit="f") as pbar:
+        while success:
+            # update the progress bar
+            pbar.update(1)
+
+            # convert OpenCV image to PIL and to greyscale
+            frame = convertOpenCV2PIL(frame).convert("L")
+
+            # convert image to ascii
+            ascii_img = convertImageToAscii(
+                frame, colors, cols, scale, moreLevels, invert
+            )
+
+            # convert ascii frame from PIL to OpenCV and add to the video
+            ascii_img_cv = convertPIL2OpenCV(ascii_img)
+            video.write(ascii_img_cv)
+
+            # get next frame if available
+            success, frame = cam.read()
+            if not success:
+                break
+
+            # if frame_num <= 3:
+            #     ascii_img.show()
+            #     frame.show()
+            # print(frame_num)
+            # frame_num += 1
+
+    # Release all space and windows once done
+    cam.release()
+    video.release()
+    cv2.destroyAllWindows()
+
+
 def test():
     filename = Path("data/test_video.mp4")
+    out = Path("out/video/test1.mp4")
+
     frames_out = Path("out/video/frames/")
     ascii_out = Path("out/video/ascii/")
-    vid_out = Path("out/video/test.mp4")
-    fps = 24
+    vid_out = Path("out/video/eyes_ascii.mp4")
+    fps = 18
 
-    # ------------------------------------------------------------------#
+    print("Converting video to ascii...")
+    start = time.perf_counter()
+
+    # Check if file given is valid
+    if not filename.exists():
+        print("ERROR: Video does not exist.")
+        exit(0)
+
+    # Convert video to ascii
+    convertVideoToAscii(filename, out)
+
+    end = time.perf_counter()
+    print(f"Completed {end - start:0.4f} seconds")
+
+    # # ------------------------------------------------------------------#
 
     # print("Converting video to frames...")
     # start = time.perf_counter()
@@ -81,45 +167,45 @@ def test():
     # end = time.perf_counter()
     # print(f"Completed {end - start:0.4f} seconds")
 
-    # ------------------------------------------------------------------#
+    # # ------------------------------------------------------------------#
 
-    print("Converting frames to ASCII...")
-    start = time.perf_counter()
+    # print("Converting frames to ASCII...")
+    # start = time.perf_counter()
 
-    # Create the frame dir if does not exist
-    ascii_out.mkdir(parents=True, exist_ok=True)
+    # # Create the frame dir if does not exist
+    # ascii_out.mkdir(parents=True, exist_ok=True)
 
-    # Get all the frame paths
-    frames = sorted(frames_out.glob("*"))
+    # # Get all the frame paths
+    # frames = sorted(frames_out.glob("*"))
 
-    for frame in frames:
-        # convert image to ascii txt
-        aimg, cimg = covertImageToAscii(
-            str(frame), color_schemes["grayscale_5"], 130, 0.5, True, True
-        )
+    # for frame in frames:
+    #     # convert image to ascii txt
+    #     aimg, cimg = covertImageToAscii(
+    #         str(frame), color_schemes["grayscale_5"], 120, 0.5, True, True
+    #     )
 
-        # make image from text
-        image = text_image(aimg, cimg, True)
+    #     # make image from text
+    #     image = text_image(aimg, cimg, True)
 
-        # save image
-        img_name = ascii_out / "{}_ascii.png".format(frame.stem)
-        image.save(img_name)
+    #     # save image
+    #     img_name = ascii_out / "{}_ascii.png".format(frame.stem)
+    #     image.save(img_name)
 
-    end = time.perf_counter()
-    print(f"Completed {end - start:0.4f} seconds")
+    # end = time.perf_counter()
+    # print(f"Completed {end - start:0.4f} seconds")
 
-    # ------------------------------------------------------------------#
+    # # ------------------------------------------------------------------#
 
-    print("Converting frames to video...")
-    start = time.perf_counter()
+    # print("Converting frames to video...")
+    # start = time.perf_counter()
 
-    # Convert frames to video
-    img_to_vid(ascii_out, vid_out, fps)
+    # # Convert frames to video
+    # img_to_vid(ascii_out, vid_out, fps)
 
-    end = time.perf_counter()
-    print(f"Completed {end - start:0.4f} seconds")
+    # end = time.perf_counter()
+    # print(f"Completed {end - start:0.4f} seconds")
 
-    # ------------------------------------------------------------------#
+    # # ------------------------------------------------------------------#
 
 
 # call main
