baef5f7ffa403127d18febd8dbf8216a7ebf4ec2
Arian Omidi <arian.omidi@icloud.com>
2021-05-08 18:08:01 -0600

    Added greyscale color palatte selector

--- color_extraction.py
+++ color_extraction.py
@@ -12,7 +12,7 @@ def RGB2HEX(color):
 
 
 def GRAY2HEX(color):
-    return "#{0:02x}{0:02x}{0:02x}".format(int(color))
+    return "#{0:02x}{0:02x}{0:02x}".format(color)
 
 
 def get_image(image_path):
@@ -21,6 +21,41 @@ def get_image(image_path):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     return image
+
+
+def getGreyscalePalatte(image, number_of_colors, show_chart=False):
+    modified_image = cv2.resize(image, (600, 400), interpolation=cv2.INTER_AREA)
+    modified_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    modified_image = modified_image.reshape(-1, 1)
+
+    clf = KMeans(n_clusters=number_of_colors)
+    labels = clf.fit_predict(modified_image)
+
+    counts = Counter(labels)
+    # sort to ensure correct color percentage
+    counts = dict(sorted(counts.items()))
+
+    center_colors = clf.cluster_centers_.flatten()
+    center_colors = np.rint(center_colors)
+    center_colors = center_colors.astype(int)
+    # We get ordered colors by iterating through the keys
+    ordered_colors = [center_colors[i] for i in counts.keys()]
+    hex_colors = [GRAY2HEX(ordered_colors[i]) for i in counts.keys()]
+    rgb_colors = [ordered_colors[i] for i in counts.keys()]
+
+    if show_chart:
+        plt.figure(figsize=(10, 6))
+
+        plt.subplot(1, 2, 1)
+        gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        plt.imshow(gray_img, cmap="gray")
+
+        plt.subplot(1, 2, 2)
+        plt.pie(counts.values(), labels=hex_colors, colors=hex_colors)
+
+        plt.show()
+
+    return rgb_colors
 
 
 def get_colors(image, number_of_colors, show_chart=False, greyscale=False):
